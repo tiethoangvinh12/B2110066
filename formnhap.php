202f159eb928eee24fbb064ebfa09e33f85f5fb6
tiethoangvinh12 <149488184+tiethoangvinh12@users.noreply.github.com>
2024-03-04 19:59:55 +0700

    Add files via upload

---
+++ formnhap.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="luu.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Birthday: <input type="date" name="birth"><br>
+Mã chuyên ngành: <input type='text' name="idmajor"><br>
+Chuyên ngành: <input type='text' name="major"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
